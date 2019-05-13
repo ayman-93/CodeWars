96726e8c300116aeb938fc3ca3d8f06fde961fc8
ayman-93 <aymanfaisal.cs@gmail.com>
2019-05-13 23:30:33 +0300

    Orgnize the files

---
+++ JS/abbrevName.js
@@ -0,0 +1,20 @@
+function abbrevName(name) {
+    let short ='';
+    for(let litter=0; litter<name.length;litter++){
+        if(litter === 0)
+            short += name[litter].toUpperCase()+".";
+        if (name[litter] === ' ')
+            short += name[litter+1].toUpperCase();
+
+    }
+return short;
+}
+
+console.log( abbrevName("Sam Harris"), "S.H");
+
+ console.log( abbrevName(("Patrick Feenan"), "P.F") );
+console.log( abbrevName("Evan Cole"), "E.C");
+
+ console.log( abbrevName("P Favuzzi"), "P.F");
+
+ console.log( abbrevName("david mendieta"));
